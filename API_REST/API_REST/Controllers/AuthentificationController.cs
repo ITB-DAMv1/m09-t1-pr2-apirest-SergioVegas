f1c727c8767a7d75798b3a120fd18abfbcc9798c
Sergio Vegas <sergio.vegas.7e8@itb.cat>
2025-04-08 13:39:58 +0200

    Authentification controller fet

--- API_REST/API_REST/Controllers/AuthentificationController.cs
+++ API_REST/API_REST/Controllers/AuthentificationController.cs
@@ -1,6 +1,11 @@
-﻿using API_REST.Model;
+﻿using API_REST.DTO;
+using API_REST.Model;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace API_REST.Controllers
 {
@@ -32,7 +37,79 @@ namespace API_REST.Controllers
 
                 return BadRequest(resultat.Errors);
             }
+            [HttpPost("admin/registre")]
+            public async Task<IActionResult> AdminRegister([FromBody] RegisterDTO model)
+            {
+                var usuari = new ApplicationUser { UserName = model.Name, Email = model.Email };
+                var resultat = await _userManager.CreateAsync(usuari, model.Password);
+                var resultatRol = new IdentityResult();
+
+                if (resultat.Succeeded)
+                {
+                    resultatRol = await _userManager.AddToRoleAsync(usuari, "Admin");
+                }
+
+                if (resultat.Succeeded && resultatRol.Succeeded)
+                    return Ok("Usuari registrat");
+
+                return BadRequest(resultat.Errors);
+            }
+
+            [HttpPost("login")]
+            public async Task<IActionResult> Login([FromBody] LoginDTO model)
+            {
+                //Certifiquem que el mail existeix
+                var usuari = await _userManager.FindByEmailAsync(model.Email);
+                if (usuari == null || !await _userManager.CheckPasswordAsync(usuari, model.Password))
+                    return Unauthorized("Mail o contrasenya erronis");
+
+                var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, usuari.UserName),
+                new Claim(ClaimTypes.NameIdentifier, usuari.Id.ToString())
+            };
+
+                //Adquirim els rols de l'usuari. Pot tenir mes d'un. En aquest cas 1.
+                var roles = await _userManager.GetRolesAsync(usuari);
+
+                if (roles != null && roles.Count > 0)
+                {
+                    foreach (var rol in roles)
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, rol));
+                    }
+                }
+
+                return Ok(CreateToken(claims.ToArray()));
+            }
+
+            private string CreateToken(Claim[] claims)
+            {
+                // Carreguem les dades des del appsettings.json
+                var jwtConfig = _configuration.GetSection("JwtSettings");
+                var secretKey = jwtConfig["Key"];
+                var issuer = jwtConfig["Issuer"];
+                var audience = jwtConfig["Audience"];
+                var expirationMinutes = int.Parse(jwtConfig["ExpirationMinutes"]);
+
+                // Creem la clau i les credencials de signatura
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                // Construcció del token
+                var token = new JwtSecurityToken(
+                    issuer: issuer,
+                    audience: audience,
+                    claims: claims,
+                    expires: DateTime.UtcNow.AddMinutes(expirationMinutes),
+                    signingCredentials: creds
+                );
+
+                // Retornem el token serialitzat
+                return new JwtSecurityTokenHandler().WriteToken(token);
+            }
         }
+    }
 
     }
 }
