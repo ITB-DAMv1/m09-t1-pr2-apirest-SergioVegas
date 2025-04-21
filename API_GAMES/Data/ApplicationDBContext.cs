e873f1e8c297798984dc6b3ddc512fae5409df8e
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-04-22 00:18:10 +0200

    dbcontext afegit

---
+++ API_GAMES/Data/ApplicationDBContext.cs
@@ -0,0 +1,31 @@
+﻿using API_REST.Model;
+using Azure;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+
+namespace API_REST.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<ApplicationUser> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Game>()
+            .HasMany(e => e.Users)
+            .WithMany(e => e.GamesU);
+
+            modelBuilder.Entity<ApplicationUser>()
+           .HasMany(e => e.GamesU)
+           .WithMany(e => e.Users);
+        }
+    }
+}
