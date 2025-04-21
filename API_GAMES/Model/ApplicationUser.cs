88cbaa7116da660a9c83037a03bbe50342f0fa4d
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-04-22 00:11:19 +0200

    Models afegits

---
+++ API_GAMES/Model/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace API_REST.Model
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public virtual ICollection<Game> GamesU { get; set; }
+    }
+}
--- API_GAMES/obj/Debug/net9.0/API_GAMES.AssemblyInfo.cs
+++ API_GAMES/obj/Debug/net9.0/API_GAMES.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_GAMES")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44b8f18ccf19d1737542b49bfdeab8bc6f5b1701")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f13558ccac900ce9176e4819acd0e9e25ddd9b8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_GAMES")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_GAMES")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
