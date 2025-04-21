13907f8aa201e04b4335b8fe97f5ea733804e1db
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-04-22 01:24:13 +0200

    Xat funcional

---
+++ CLIENT_GAMES/Pages/UserXat.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CLIENT_GAMES.Pages
+{
+
+    public class UserXatModel : PageModel
+    {
+        
+        
+    }
+}
